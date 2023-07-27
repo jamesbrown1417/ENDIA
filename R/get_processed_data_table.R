24e30f68337ac1d42214aa59b8992225cb1fc733
James Brown <james.brown1417@gmail.com>
2023-07-27 16:57:09 +0930

    fixed get processed data to include excel files

--- R/get_processed_data_table.R
+++ R/get_processed_data_table.R
@@ -9,11 +9,20 @@
 #'
 #' @examples
 #' get_processed_data_table("mother_ids")
-get_processed_data_table <-
-    function(table_name, cols, include_table_name = FALSE) {
-        # Path to the processed data folder on the ENDIA shared drive
-        processed_path = "S:/HealthSciences/SPRH/Paediatrics/Diabetes Research Group/Statistics and Data Management/Data/Processed/"
+get_processed_data_table <- function(table_name, cols, include_table_name = FALSE, sheet = NULL) {
+    # Path to the processed data folder on the ENDIA shared drive
+    processed_path = "S:/HealthSciences/SPRH/Paediatrics/Diabetes Research Group/Statistics and Data Management/Data/Processed/"
 
+    # list files matching csv or xlsx in processed path
+    file_list <- list.files(processed_path, pattern = "(\\.csv)|(\\.xlsx)")
+
+    # Match table name with list
+    format_string <- file_list[stringr::str_detect(file_list, table_name)][1]
+
+    # Identify the file format
+    file_format <- tools::file_ext(format_string)
+
+    if(file_format == "csv") {
         # combine file name and path to get location of desired file to read in
         file_path = base::paste0(processed_path, table_name, ".csv")
 
@@ -34,12 +43,27 @@ get_processed_data_table <-
                 show_col_types = FALSE
             )
         }
+    } else if(file_format == "xlsx") {
+        # combine file name and path to get location of desired file to read in
+        file_path = base::paste0(processed_path, table_name, ".xlsx")
 
-        # If table_name argument is TRUE add table name variable to output
-        if (include_table_name) {
-            output_data$table_name <- paste0(table_name)
+        # If cols argument supplied, return a warning
+        if (!missing(cols)) {
+            warning("Column selection is not supported for xlsx files.")
         }
 
-        # Return the table
-        return(output_data)
+        # Read the excel file
+        output_data <- readxl::read_excel(file_path, sheet = sheet)
+
+    } else {
+        stop("Unsupported file format!")
     }
+
+    # If table_name argument is TRUE add table name variable to output
+    if (include_table_name) {
+        output_data$table_name <- paste0(table_name)
+    }
+
+    # Return the table
+    return(output_data)
+}
