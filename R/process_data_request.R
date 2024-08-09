de01fd4ee33ac01eb73686dd7d8675461947cd04
jamesbrown1417 <james.brown1417@gmail.com>
2024-08-09 16:38:30 +0930

    fixed no visits selected issue

--- R/process_data_request.R
+++ R/process_data_request.R
@@ -24,6 +24,37 @@ process_data_request <- function(data_request_filepath) {
         dplyr::pull(Visit) |>
         ENDIA::fix_visit_numbers(how = "both")
 
+    # If no visits are selected, return all visits
+    if (is.null(nrow(visits_requested))) {
+        visits_requested <-
+            ENDIA::fix_visit_numbers(c(
+            "v1",
+            "v2",
+            "v3",
+            "v4",
+            "v5",
+            "v6",
+            "v7",
+            "v8",
+            "v9",
+            "v10",
+            "v11",
+            "v12",
+            "v13",
+            "v14",
+            "v15",
+            "v16",
+            "v17",
+            "v18",
+            "v19",
+            "v20",
+            "v21",
+            "v22",
+            "v23",
+            "v24"
+        ), how = "both")
+    }
+
     # Clinical
     clinical_variables_requested <-
         clinical_variables |>
